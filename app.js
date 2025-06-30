96d48d15e6e6d2e9a02596ddfd4f1239c8b21937
Udanth <udanth.work@gmail.com>
2025-06-30 21:34:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const ejs = require('ejs')
+const Listing = require("./models/listing.js")
+const app = express()
+const path = require("path")
+
+const MONGO_URL = ""
+
+main()
+    .then(()=>{
+            console.log("Connected to DB")
+    })
+    .catch((err)=>{
+            console.log(err)
+    })
+
+async function main(){
+   await mongoose.connect(MONGO_URL)
+}
+
+app.set("view engine", 'ejs')
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({extended:true}))
+
+app.get("/", (req, res) => {
+    res.send("Hi I am root")
+})
+
+app.get('/listings', async(req, res) => {
+   const allListings =  await Listing.find({})
+   res.render("listings/index", { allListings });
+})
+
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new")
+})
+
+app.get("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id)
+    res.render("listings/show",{listing})
+})
+
+app.post("/listings",async(req,res)=>{
+    const newListing = new Listing(req.body.listing)
+    await newListing.save()
+    res.redirect("/listings")
+})
+
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id)
+    res.render("listings/edit",{listing})
+})
+
+// app.get("/testListing",async(req,res)=>{
+//     let sampleListing = new Listing({
+//         title : "My Home",
+//         description : "By the beach",
+//         price : 1000,
+//         location : "Calangute, Goa",
+//         country : "India"
+//     })
+//     await sampleListing.save()
+//     console.log("sample listing was saved")
+//     res.send("succesful testing")
+// })
+
+app.listen(8080, () => {
+    console.log("Server is listening to port 8080")
+})
---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    description : String,
+    image : {
+        type : String,
+        default : "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D",
+        set:(v) => v === "" ? "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmVhY2glMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D" : v,
+        
+    },
+    price : Number,
+    location : String,
+    country : String
+})
+
+const Listing = mongoose.model("Listing",listingSchema)
+module.exports = Listing
