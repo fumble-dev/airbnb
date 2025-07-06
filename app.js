c5d11f0ee1dd4b17906116af2cd584b27c5b2735
Udanth <udanth.work@gmail.com>
2025-07-06 06:55:28 +0530

    added validation middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const wrapAsync = require('./utils/wrapAsync.js')
 const ExpressError = require('./utils/ExpressError.js')
+const listingSchema = require("./schema.js")
 
 const MONGO_URL = ""
 
@@ -34,6 +35,16 @@ app.get("/", (req, res) => {
     res.send("Hi I am root")
 })
 
+const validateListing = (req, res, next) => {
+    const { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((e)=>e.message).join(",")
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
 app.get('/listings', wrapAsync(async (req, res) => {
     const allListings = await Listing.find({})
     res.render("listings/index", { allListings });
@@ -49,10 +60,7 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
     res.render("listings/show.ejs", { listing })
 }))
 
-app.post("/listings", wrapAsync(async (req, res, next) => {
-    if(!req.body.listing){
-        throw new ExpressError(400,"Send Valid data for listing")
-    }
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing)
     await newListing.save()
     res.redirect("/listings")
@@ -64,7 +72,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     res.render("listings/edit", { listing })
 }))
 
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing })
     res.redirect(`/listings/${id}`);
@@ -77,30 +85,10 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     res.redirect('/listings')
 }))
 
-
-
-// app.get("/testListing",async(req,res)=>{
-//     let sampleListing = new Listing({
-//         title : "My Home",
-//         description : "By the beach",
-//         price : 1000,
-//         location : "Calangute, Goa",
-//         country : "India"
-//     })
-//     await sampleListing.save()
-//     console.log("sample listing was saved")
-//     res.send("succesful testing")
-// })
-
-
-// app.all("*", (req, res, next) => {
-//     const err = new ExpressError(404, "Page Not Found !");
-//     next(err);
-// });
-
 app.use((err, req, res, next) => {
     const { statusCode = 500, message = "Something went wrong!" } = err;
-    res.status(statusCode).send(message);
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", { err })
 });
 
 
