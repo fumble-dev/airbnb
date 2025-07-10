c31bd6d85a979b66cc1211d36a2be3bdb57d1cb8
Udanth <udanth.work@gmail.com>
2025-07-10 08:49:41 +0530

    added user signup

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const app = express()
 const path = require("path")
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
-const listings = require('./routes/listing.js')
-const reviews = require('./routes/review.js')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
+const localStrategy = require('passport-local')
+const User = require('./models/user.js')
+
+const listingRouter = require('./routes/listing.js')
+const reviewRouter= require('./routes/review.js')
+const userRouter = require('./routes/user.js');
 
 const MONGO_URL = ""
 
@@ -31,31 +36,49 @@ app.engine('ejs', ejsMate)
 app.use(express.static(path.join(__dirname, "/public")))
 
 const sessionOptions = {
-    secret : "secretcode",
-    resave : false,
-    saveUninitialized : true,
-    cookie : {
-        expires : Date.now() * 7 * 24 * 60 * 60 * 1000,
-        maxAge : 7 * 24 * 60 * 60 * 1000,
-        httpOnly : true
+    secret: "secretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() * 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true
     },
 }
 
 app.use(session(sessionOptions))
 app.use(flash())
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.get("/", (req, res) => {
     res.send("Hi I am root")
 })
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success")
     res.locals.error = req.flash("error");
     next();
 })
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews)
+app.get("/demouser",async(req,res)=>{
+    let fakeUser = new User({
+        email : "demo@gmail.com",
+        username : "udanth"
+    })
+   const registeredUser = await User.register(fakeUser,"hello");
+   res.send(registeredUser);
+})
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter)
+app.use('/', userRouter);
+
 
 app.use((err, req, res, next) => {
     const { statusCode = 500, message = "Something went wrong!" } = err;
