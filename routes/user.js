a033044d37ae2ac8d4f3d8085b7e3c9bc18de536
Udanth <udanth.work@gmail.com>
2025-07-10 09:29:56 +0530

    added login

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const wrapAsync = require('../utils/wrapAsync');
 const router = express.Router();
 const User = require('../models/user.js');
+const passport = require('passport');
 
 router.get('/signup',(req,res)=>{
     res.render('users/signup.ejs')
@@ -19,6 +20,19 @@ router.post('/signup',wrapAsync(async(req,res)=>{
         req.flash('error',e.message);
         res.redirect('/signup')
     }
+}))
+
+router.get('/login',(req,res)=>{
+    res.render('users/login.ejs');
+})
+
+router.post('/login',
+    passport.authenticate('local',{
+        failureRedirect :'/login', 
+        failureFlash : true})
+        ,wrapAsync(async(req,res)=>{
+            req.flash("success","Login Successful")
+            res.redirect("/listings");
 }))
 
 module.exports = router;
